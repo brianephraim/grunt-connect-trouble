71025fe35b90b25407c82547a31bd43e0d346833
Brian Ephraim <brian@okcupid.com>
2014-04-09 16:53:23 -0400

    asdf

---
+++ Gruntfile.js
@@ -0,0 +1,155 @@
+'use strict';
+
+module.exports = function(grunt) {
+  //mac uses 0.0.0.0 to share localhost ports, but on windows it breaks, so use localhost on windows
+  var myHost = !!process.platform.match(/^win/) ? 'localhost' : '0.0.0.0';
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+
+
+
+
+
+  // Project configuration.
+  grunt.initConfig({
+
+
+    // The actual grunt server settings
+    connect: {
+      options: {
+        port: 9001,
+        // Change this to '0.0.0.0' to access the server from outside.
+        //on mac it's 0.0.0.0.  on windows it's localhost.  because windows fails with 0.0.0.0
+        hostname: myHost,
+        // livereload: 35729,
+      },
+      yeoman: {
+        // configurable paths
+        app: './app',
+        distBaseUrl: 'http://s3.amazonaws.com/datehookup/dhspa/deploy/<%= yeoman.distBranch %>/dist/app/',
+        distBranch: 'junk'
+      },
+      proxies: [
+        {
+            context: '/qwer',
+            host: myHost,
+            port: '8086',
+            //ws: true,
+            https: false,
+            changeOrigin: true,
+            //xforward: false,
+            headers: {
+                "x-custom-added-header": 'asdfasdfasdfasdf'
+            }
+        },
+        {
+            context: '/zxcv',
+            host: myHost,
+            port: '8085',
+            ws: true,
+            https: false,
+            //changeOrigin: true,
+            //xforward: false,
+            // rewrite: {
+            //     '^/qwer': '/asdf'
+            // },
+            headers: {
+                "x-custom-added-header": 'asdfasdfasdfasdf'
+            }
+        },
+        {
+            context: '/signalr',
+            //host: 'tj-pc.office.okcupid.com',
+            host: 'test.datehookup.com',
+
+            //port: '8085',
+            //ws: true,
+            https: false,
+            changeOrigin: true,
+            //xforward: false,
+            // rewrite: {
+            //     '^/qwer': '/asdf'
+            // },
+            headers: {
+                "x-custom-added-header": 'asdfasdfasdfasdf'
+            }
+        }
+      ],
+      imgserver: {
+        options: {
+          port: 9010,
+          base: [
+            '.tmp',
+            './app'
+          ]
+        }
+      },
+      livereload: {
+        options: {
+          //open: 'http://'+myHost+':9000/dev.html',
+          base: [
+            '.tmp',
+            './app'
+          ],
+
+
+          middleware: function (connect, options) { //PROXY STUFF
+              var middlewares = [];
+              var directory = options.directory || options.base[options.base.length - 1];
+              if (!Array.isArray(options.base)) {
+                  options.base = [options.base];
+              }
+                           // Setup the proxy
+                           middlewares.push(require('grunt-connect-proxy/lib/utils').proxyRequest);
+
+              options.base.forEach(function(base) {
+                  // Serve static files.
+                  middlewares.push(connect.static(base));
+              });
+
+              // Make directory browse-able.
+              middlewares.push(connect.directory(directory));
+
+              return middlewares;
+          }
+        }
+      }
+
+
+    }
+
+
+
+
+  });
+  //CONFIG ABOVE
+  //===================
+  //===================
+  //===================
+  //===================
+  //===================
+  //===================
+  //===================
+
+grunt.registerTask('serve', function (target) {
+    console.log('applicationServer')
+    grunt.task.run([
+      // 'configureProxies',
+      'connect:livereload',
+      'connect:imgserver',
+      // 'watch'
+    ]);
+  });
+
+
+
+  // Default task.
+  grunt.registerTask('default', ['serve']);
+
+
+
+    grunt.registerTask('revert', ['shell:gitRevertCommit']);
+
+
+};
